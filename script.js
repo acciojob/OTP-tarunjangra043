e9e8fa9a7902dc425e616e63fbf950d5555787d5
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-02 12:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const codes = document.querySelectorAll('.code');
+const codes = document.querySelectorAll(".code")
 
-    codes.forEach((code, index) => {
-        code.addEventListener('input', (e) => {
-            if (e.inputType === 'deleteContentBackward') {
-                if (index > 0) {
-                    codes[index - 1].focus();
-                }
-            } else {
-                if (index < codes.length - 1) {
-                    codes[index + 1].focus();
-                }
-            }
-        });
+codes[0].focus()
 
-        code.addEventListener('keydown', (e) => {
-            if (e.key === 'Backspace' && !code.value) {
-                if (index > 0) {
-                    codes[index - 1].focus();
-                }
+codes.forEach((code, idx) => {
+    code.addEventListener("keydown", (e) => {
+        console.log(e.key)
+        const keyPressed = e.key
+
+        if (keyPressed >= 0 && keyPressed <= 9) {
+            codes[idx].value = ''
+            if (idx < codes.length - 1) {
+                setTimeout(() => codes[idx + 1].focus(), 10)
+
+                // setTimeout(function(){
+                //     codes[index+1].focus()
+                // },10)
             }
-        });
-    });
-});
+        }
+        else if (keyPressed === "Backspace") {
+            if (idx > 0) {
+                setTimeout(() => codes[idx - 1].focus(), 10)
+            }
+        }
+    })
+})
